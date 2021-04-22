bbf9e978be2a0c4668ea7c592bfc0d97d7a7e353
ljk1256 <ljk1256@naver.com>
2021-04-22 12:38:36 +0900

    try 1

--- src/midTermExam/strassen.java
+++ src/midTermExam/strassen.java
@@ -1,6 +1,14 @@
 package midTermExam;
 
 public class strassen {
+	public int divideMatrix(int[][] matrix) {
+		
+	}
+	
+	public int initMatrix(int number) {
+		return matrix;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
